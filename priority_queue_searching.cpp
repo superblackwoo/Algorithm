3e5031787b2bdccdf868aca30c11b8d2426462cc
superblackwoo <54014116+superblackwoo@users.noreply.github.com>
2019-11-22 15:43:20 +0800

    Add files via upload

---
+++ priority_queue_searching.cpp
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#define MAX_LINE 1024
+#define MAX_NUM  1024
+int read(int numbers[])  // read data in file, write data in number's' []
+{
+	int  number[MAX_LINE] = { 0 }, i = 0, j = 1;
+	for (i = 0; i < MAX_LINE; i++) {	//初始化
+		number[i] = MAX_NUM;
+	}  
+	FILE *fp;
+	if((fp = fopen("G:\\a大三上\\算法分析与设计\\算法实验\\实验二\\input-1.txt","r")) == NULL){
+		printf("Open file failed\n");
+	}
+	i = 0;
+	printf("input1: ");
+	while (fscanf(fp, "%d", &number[i])!=EOF) {  // file writ in the arrary number[] first
+		printf("%d  ", number[i]);
+		i++; //		the location of last 
+	}
+	for (i = 0; i < MAX_LINE; i++) {
+		if (number[i] == MAX_NUM)
+			break;
+		numbers[j] = number[i];
+		++j;  // j initlize j = 1. So j is The size of numbers[]
+			  //                      
+	}
+	//for (i = 0; i < j; i++)
+		//printf("*%d ", numbers[i]);
+	//printf("\n");
+	if (fclose(fp) != 0)
+		printf("file can't close\n");
+	return j;
+}
+void write(int numbers[], int size)  // size: the size of numbers[]
+{
+	int i ;
+	FILE *fp;
+	if((fp = fopen("G:\\a大三上\\算法分析与设计\\算法实验\\实验二\\output-1.txt","w")) == NULL){
+		printf("Open file failed\n");
+	}
+	fprintf(fp,"%d   ",numbers[1]);
+	for(i=1;i<size;i++){
+		fprintf(fp, "%d, ", numbers[i]);
+	}
+	if (fclose(fp) != 0)
+		printf("file can't close\n");
+}
+
+
+void HeapAdjust(int numbers[], int s, int n)
+{
+	int i, temp;
+	temp = numbers[s];
+	for(i=2*s; i<=n; i*=2){
+		/* i永远指向最大的子节点 */ 
+		if(i<n && numbers[i] < numbers[i+1]) i++;	//右子树大于左子节点 i指向右子节点
+		if(temp >= numbers[i]) break;		//双亲已经大于了最大子节点 
+		numbers[s] = numbers[i];
+		s = i;
+	}
+	numbers[s] = temp;
+}
+
+void priority_q(int numbers[], int n)
+{
+	int i;
+	for(i=n/2; i>0; i--){  //从下至上，从右到左构建大顶堆 
+		HeapAdjust(numbers, i, n);
+	}
+	
+}
+
+
+int main()
+{
+	int i, numbers[MAX_LINE] = {0}, high; // high is down flag of the last numbers[last] 
+    high = read(numbers) - 1;
+    priority_q(numbers,high);
+    write(numbers, high+1);
+    printf("\n\nAfter  HeapSorting the priority queue sort is :\n");
+    printf("%d   ",numbers[1]);
+    for(i=1;i<high+1;i++)
+    	printf("%d,  ",numbers[i]);
+    printf("\n");
+    return 0;
+}
